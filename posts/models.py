823b8f79c2042bd3858041a139717e31a52c78a4
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-15 11:05:08 -0800

    Allow users to add message

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     user = models.CharField(max_length=30)
     message = models.TextField(max_length=250)
+    post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user + ": " + self.message[:20]
-
+    
+    def get_absolute_url(self):
+        return reverse("home")
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
 urlpatterns = [
+    path("new/", PostCreateView.as_view(), name="new_message"),
     path("", PostListView.as_view(), name="home")
 ]
